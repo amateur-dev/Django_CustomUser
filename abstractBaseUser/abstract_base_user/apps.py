6bb7a8676c48d80c0817a164ca801a1008e874dc
amateur-dev <dipesh.sukhani@gmail.com>
2019-05-16 19:33:50 +0800

    AbstractBaseUser project achieved

---
+++ abstractBaseUser/abstract_base_user/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AbstractBaseUserConfig(AppConfig):
+    name = 'abstract_base_user'
---
+++ abstractBaseUser/abstract_base_user/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('email', 'f_name', 'l_name', 'condo_name', 'unit_floor', 'unit_unit')
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'f_name', 'l_name', 'condo_name', 'unit_floor', 'unit_unit')
